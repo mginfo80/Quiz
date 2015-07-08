211ecae7fc96f17acad04cb14f3d293decb2b646
mginfo80 <gorski.marcelo@gmail.com>
2015-07-08 20:15:41 -0300

    CSS adaptable

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-
-var app = express();
+var express 		= require('express');
+var path 			= require('path');
+var favicon 		= require('serve-favicon');
+var logger 			= require('morgan');
+var cookieParser 	= require('cookie-parser');
+var bodyParser 		= require('body-parser');
+var routes 			= require('./routes/index');
+var app 			= express();
+var partials		= require('express-partials');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,8 +19,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', routes);
+app.use(partials());  // Marco de la aplicación
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
