ed78d9fd011c2ea38e0145e3d5aa164a75b7a1f1
mginfo80 <gorski.marcelo@gmail.com>
2015-07-09 11:01:49 -0300

    Responsivo

--- app.js
+++ app.js
@@ -4,14 +4,15 @@ var favicon 		= require('serve-favicon');
 var logger 			= require('morgan');
 var cookieParser 	= require('cookie-parser');
 var bodyParser 		= require('body-parser');
+var partials		= require('express-partials');
 var routes 			= require('./routes/index');
 var app 			= express();
-var partials		= require('express-partials');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(partials());  // Marco de la aplicación
 // uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
@@ -20,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
-app.use(partials());  // Marco de la aplicación
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
